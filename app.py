07a05c0797930fc243b124e432a0c14ffb534f2e
Artem <artnicanov@gmail.com>
2022-10-09 01:45:10 +0300

    api endpoints and logger added, search view updated,

--- app.py
+++ app.py
@@ -1,39 +1,81 @@
-from flask import Flask, render_template, request
+import logging
+from flask import Flask, render_template, request, jsonify
 import utils
 
+logging.basicConfig(filemode='w', level=logging.INFO)
+api_logger = logging.getLogger()  # Создаем логгер
+file_handler = logging.FileHandler("api.log")  # Cоздаем ему обработчик для записи в файл
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")  # Создаем новое форматирование
+api_logger.addHandler(file_handler)  # Добавляем обработчик к журналу
+file_handler.setFormatter(formatter)  # Применяем форматирование к обработчику
+
 app = Flask(__name__)
+
 
 # вьюшка ленты со всем постами
 @app.route('/')
 def main_page():
-	posts_data = utils.get_posts_all() # в эту переменную передаем все посты
+	posts_data = utils.get_posts_all()  # в эту переменную передаем все посты
 	return render_template('index.html', posts=posts_data)
 
+
 # вьюшка для одного поста
-@app.route('/posts/<postid>')
+@app.route('/posts/<int:postid>')  # id поста обязательно привести к числу, иначе будет ошибка
 def post_page(postid):
-	postid = int(postid) # id поста обязательно привести к числу, иначе будет ошибка
-	post_data = utils.get_post_by_pk(postid) # в эту переменную передаем конкретный пост по его идентификатору
+	post_data = utils.get_post_by_pk(postid)  # в эту переменную передаем конкретный пост по его идентификатору
 	try:
 		comments_data = utils.get_comments_by_post_id(postid)  # в эту переменную передаем все комментарии к выбранному посту
-		comments_count = len(comments_data) # здесь колличество комментариев
+		comments_count = len(comments_data)  # здесь колличество комментариев
 	except ValueError:
-		return "POST NOT FOUND" # покажем это сообщение если такого поста нет
+		return "POST NOT FOUND"  # покажем это сообщение если такого поста нет
 	return render_template('post.html', post=post_data, comments=comments_data, comments_count=comments_count)
 
-# вьюшка для поиска пока не работает приходится вручную прописывать ?s=
+
+# вьюшка для поиска
 @app.route('/search/')
 def search_page():
-	search_query = request.args.get('s') # получаем значение из адресной строки через args
-	posts_data = utils.search_for_posts(str(search_query)) # передаем функции, которая ищет слова, значение полученное строкой выше
+	search_query = request.args.get('s')  # получаем значение из адресной строки через args
+	posts_data = utils.search_for_posts(
+		search_query)  # передаем функции, которая ищет слова, значение полученное строкой выше
 	posts_count = len(posts_data)
-	return render_template('search.html', query=search_query, posts=posts_data, posts_count=posts_count)
+	return render_template('search.html', posts=posts_data, posts_count=posts_count)
+
 
 # вьюшка с выводом постов конкретного пользователя
 @app.route('/users/<username>')
 def user_posts(username):
-	posts_data = utils.get_posts_by_user(username)
+	try:
+		posts_data = utils.get_posts_by_user(username)
+	except ValueError:
+		return "USER NOT FOUND"  # покажем это сообщение если такого пользователя нет
+
 	return render_template('user-feed.html', posts=posts_data)
+
+
+# вьюшка для обработки запросов к несуществующим страницам
+@app.errorhandler(404)
+def no_such_page(error):
+	return "NO SUCH PAGE"
+
+# вьюшка для обработки ошибок на сервере
+@app.errorhandler(500)
+def server_mistake(error):
+	return "SERVER MISTAKE"
+
+
+# вьюшка для тестов API, возвращает полный список постов в виде JSON-списка
+@app.route('/api/posts')
+def first_api_endpoint():
+	posts_data = utils.get_posts_all()  # в эту переменную передаем все посты
+	logging.info("Запрос /api/posts")  # сообщени для логгера
+	return jsonify(posts_data)
+
+# вьюшка для тестов API, возвращает один пост в виде JSON-словаря
+@app.route('/api/posts/<int:postid>')
+def second_api_endpoint(postid):
+	post_data = utils.get_post_by_pk(postid)  # в эту переменную передаем конкретный пост по его идентификатору
+	logging.info(f"Запрос /api/posts/{postid}")  # сообщени для логгера
+	return jsonify(post_data)
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -12,37 +12,41 @@ def get_comments_all():
 
 def get_posts_by_user(user_name):
 	""" возвращает посты определенного пользователя """
-	user_posts = [] # в этот список будем собирать посты конкретного пользователя
-	all_posts = get_posts_all() # обращаемся к функции, которая покажет все посты
+	user_posts = []  # в этот список будем собирать посты конкретного пользователя
+	all_posts = get_posts_all()  # обращаемся к функции, которая покажет все посты
 	user_exists = False  # эта булева переменная нужна чтобы проверить наличие пользователя и вызвать ошибку если его нет
 	for post in all_posts:
 		if user_name == post['poster_name']:
-			user_posts.append(post) # добавляем в список если комментарий есть
+			user_posts.append(post)  # добавляем в список если комментарий есть
 			user_exists = True
 	if not user_exists:
-		raise ValueError # Функция должна вызывать ошибку ValueError если такого пользователя нет
+		raise ValueError  # Функция должна вызывать ошибку ValueError если такого пользователя нет
 	return user_posts
 
 def get_comments_by_post_id(post_id):
 	"""возвращает комментарии определенного поста"""
-	comments_list = [] # в этот список будем собирать комментарии к конкретному посту
-	all_comments = get_comments_all() # обращаемся к функции, которая покажет все комментарии
-	post_exists = False # эта булева переменная нужна чтобы проверить наличие поста и вызвать ошибку если его нет
+	comments_list = []  # в этот список будем собирать комментарии к конкретному посту
+	all_comments = get_comments_all()  # обращаемся к функции, которая покажет все комментарии
+	post_exists = False  # эта булева переменная нужна чтобы проверить наличие поста и вызвать ошибку если его нет
 	for comment in all_comments:
 		if post_id == comment['post_id']:
 			post_exists = True
-			comments_list.append(comment) # добавляем в список если комментарий есть
-	# if not post_exists:
-	# 	raise ValueError # Функция должна вызывать ошибку ValueError если такого поста нет
+			comments_list.append(comment)  # добавляем в список если комментарий есть
+	if not post_exists:
+		raise ValueError  # Функция должна вызывать ошибку ValueError если такого поста нет
 	return comments_list
 
 def search_for_posts(query):
 	""" возвращает список постов по ключевому слову """
+	posts_count = 0  # счетчик для количества постов
 	posts_list_by_word = []
 	all_posts = get_posts_all()  # обращаемся к функции, которая покажет все посты
 	for post in all_posts:
+		if posts_count == 10:  # условие по которому показываются только 10 постов
+			break
 		if query.lower() in post['content'].lower():
 			posts_list_by_word.append(post)
+			posts_count += 1
 	return posts_list_by_word
 
 def get_post_by_pk(pk):
@@ -51,4 +55,3 @@ def get_post_by_pk(pk):
 	for post in all_posts:
 		if pk == post['pk']:
 			return post
-
